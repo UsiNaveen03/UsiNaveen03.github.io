dd87b252f26cdc67bf739f294ca06a74530e1ab9
Administrator <UsiNaveen03@gmail.com>
2019-08-30 16:08:12 +0530

    my first pppprrroooy

---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export  default class menu extends React.Component{
+        render(){
+              //overridesrender inside React.Component
+              return(
+                     <div>
+                       <p>usikha</p>
+                       <p>cse</p>
+                     </div>
+                    )
+       }
+}
